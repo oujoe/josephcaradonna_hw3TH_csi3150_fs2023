f1732eeec1eac9ab61df5e15f690b5c92fe07795
oujoe <josephcaradonna@oakland.edu>
2023-10-22 11:54:50 -0400

    added to all pages

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/* Hidden list functions */
+function hiddenList() {
+  var x = document.getElementById("hidden-list");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function hiddenList2() {
+  var x = document.getElementById("hidden-list2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
