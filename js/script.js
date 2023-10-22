1732498930fb7c93255047a0c1e3fa7277bdfd1b
oujoe <josephcaradonna@oakland.edu>
2023-10-22 12:02:44 -0400

    fixed button issue

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 /* Hidden list functions */
 function hiddenList() {
   var x = document.getElementById("hidden-list");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
+  if (x.style.display === "block") {
     x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
 }
 
